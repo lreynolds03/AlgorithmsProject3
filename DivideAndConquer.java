f32c8fa8c3ee52d7f951f65ad10835fd110e0ee5
mdavidson982 <90424696+mdavidson982@users.noreply.github.com>
2023-04-25 16:44:55 -0400

    Add files via upload

--- DivideAndConquer.java
+++ DivideAndConquer.java
@@ -4,17 +4,18 @@ public class DivideAndConquer {
 
 	String[] x;
 	String[] y;
-	int m, n;
+	int m, n, comparisons;
 
 	public static void main(String[] args) {
-		String[] x = { "A", "A","C","A","G","T","T","A","C","C"};
-		String[] y = { "T", "A","A","G","G","T","C","A"};
+		String[] x = { "T", "G", "T", "T", "A", "G", "C", "C", "G", "A"};
+		String[] y = { "T", "G", "T", "T", "A", "G", "C", "C", "G", "A"};
 		
 		DivideAndConquer DC = new DivideAndConquer(x, y);
 		long startTime = System.nanoTime();
 		DC.opt(0, 0);
 		long endTime = System.nanoTime();
 		System.out.println(endTime - startTime);
+		System.out.println(DC.comparisons);
 	}
 
 	public DivideAndConquer(String[] x, String[] y) {
@@ -22,20 +23,25 @@ public class DivideAndConquer {
 		this.y = y;
 		this.m = x.length;
 		this.n = y.length;
+		this.comparisons = 0;
 	}
 
 	public int opt(int i, int j) {
 		int penalty;
 		if (i == x.length) {
+			comparisons++;
 			 return 2 * (n - j);
 		} else if (j == y.length) {
+			comparisons+=2;
 			return 2 * (m - i);
 		} else {
+			comparisons+=2;
 			if (x[i] == y[j]) {
+				comparisons++;
 				penalty = 0;
 			} else {
+				comparisons++;
 				penalty = 1;
-
 			}
 			 return Math.min(opt(i + 1, j + 1) + penalty, Math.min(opt(i + 1, j) + 2, opt(i, j + 1) + 2));
 		}
--- Dynamic.java
+++ Dynamic.java
@@ -4,15 +4,17 @@ public class Dynamic {
 	String[] x,y;
 	int m,n;
 	int[][] table;
+	int comparisons;
 	public static void main(String[] args) {
-		String[] x = { "A", "A", "C", "A", "G", "T", "T", "A", "C", "C" };
-		String[] y = { "T", "A", "A", "G", "G", "T", "C", "A" };
+		String[] x = { "T", "G", "T", "T", "A", "G", "C", "C", "G", "A"};
+		String[] y = { "T", "G", "T", "T", "A", "G", "C", "C", "G", "A"};
 		Dynamic Dy = new Dynamic(x, y);
 		
 		long startTime = System.nanoTime();
 		Dy.opt();
 		long endTime = System.nanoTime();
 		System.out.println(endTime - startTime);
+		System.out.println(Dy.comparisons);
 
 	}
 	
@@ -21,6 +23,7 @@ public class Dynamic {
 		this.y = y;
 		this.m = x.length;
 		this.n = y.length;
+		this.comparisons = 0;
 		table = new int[x.length + 1][y.length + 1];
 		
 		
@@ -42,20 +45,22 @@ public class Dynamic {
 		for(int a = m-1; a>= 0; a--) {
 			for(int b = n-1; b >= 0; b--) {
 				if(x[a] == y[b]) {
+					comparisons++;
 					penalty = 0;
 				}else {
+					comparisons++;
 					penalty = 1;
 				}
 				table[a][b] = Math.min(table[a+1][b+1]+penalty,Math.min(table[a+1][b]+2, table[a][b+1]+2));
 			}
 		}
 		
-		for(int a = 0; a < table.length; a++) {
-			for(int b = 0; b < table[0].length; b++) {
+		//for(int a = 0; a < table.length; a++) {
+			//for(int b = 0; b < table[0].length; b++) {
 				//System.out.print(table[a][b] + ", ");
-			}
+			//}
 			//System.out.println();
-		}
+		//}
 		
 		return table[0][0];
 		
